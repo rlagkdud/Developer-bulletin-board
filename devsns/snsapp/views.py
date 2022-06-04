38c6260dcf5d939b5904e8ae9f7b9c0a88e873af
rlagkdud <ha0936861@gmail.com>
2022-06-04 16:05:19 +0900

    pagination

--- devsns/snsapp/views.py
+++ devsns/snsapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm, CommentForm, FreeCommentForm, FreePostForm
 from .models import Post, FreePost
+from django.core.paginator import Paginator
 
 def home(request):
     # posts = Post.objects.all()
     posts = Post.objects.filter().order_by('-date')
+    paginator=Paginator(posts,5)
+    pagenum=request.GET.get("page")
+    posts = paginator.get_page(pagenum)
     return render(request, 'index.html', {'posts':posts})
 
 def postcreate(request):
